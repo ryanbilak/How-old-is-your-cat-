682001c40b7f6437887f0e93cc9da5efbc10b339
Ryan Bilak <ryan@beachambilak.com>
2016-07-07 17:54:10 -0400

    First Commit

--- How old is your cat?/ViewController.swift
+++ How old is your cat?/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var catAgeTextField: UITextField!
+    
+    @IBOutlet var catAge: UILabel!
+    
+    @IBAction func findCatAge(sender: AnyObject) {
+        
+        var catYears = Int(catAgeTextField.text!)!
+        
+        catYears = catYears * 7
+        
+        catAge.text = "Your cat is \(catYears) in cat years"
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
